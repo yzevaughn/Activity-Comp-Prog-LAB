96a9e9a916477ff2f83a08256a49552d53dfdd57
yzevaughn <ty202500492@wmsu.edu.ph>
2026-02-16 18:45:03 +0800

    Add files via upload

---
+++ EvangelistaCC102Lab4_1.cpp.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    char choice;
+
+    do {
+
+        system("cls");
+        int numOfStudents, quizzes;
+
+        cout << "Enter number of Student: ";
+        cin >> numOfStudents;
+
+        cout << "Enter number of Quizzes: ";
+        cin >> quizzes;
+
+
+        int scores[numOfStudents][quizzes];
+        double average[numOfStudents];
+
+        cout << endl;
+
+        for (int i = 0; i < numOfStudents; i++) {
+
+            int sum = 0;
+
+            for (int j = 0; j < quizzes; j++) {
+                cout << "Student " << i + 1 << ": Enter the score of Quiz #" << i + 1 << ": ";
+                cin >> scores[i][j];
+                sum += scores[i][j];
+            }
+
+            average[i] = sum / double(quizzes);
+            cout << endl;
+        }
+
+
+        cout << "\nStudent\t";
+
+        for (int i = 0; i < quizzes; i++) {
+
+            cout << "Q" << i + 1 << "\t";
+
+        }
+
+        cout << "Average\n";
+
+        cout << "-----------------------------------------------\n";
+        for (int i = 0; i < numOfStudents; i++) {
+
+            cout << i + 1 << "\t";
+
+            for (int j = 0; j < quizzes; j++) {
+
+                cout << scores[i][j] << "\t";
+
+                }
+
+            cout << fixed << setprecision(2) << average[i] << endl;
+
+        }
+
+        cout << "\nDo you want to try again? (y/n): ";
+        cin >> choice;
+
+    } while (tolower(choice) == 'y');
+
+    return 0;
+}
